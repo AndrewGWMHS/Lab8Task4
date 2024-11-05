5f4dad6b25124bc55289750e3d16e2a5d94d1397
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-11-05 08:27:33 -0500

    1

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CheckOut {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean goContinue = false;
+        double currentPrice;
+        double totalPrice = 0;
+
+        do {
+            currentPrice = InputHelper.getRangedDouble(scan, "Enter the price of your item [0.50-9.99]", 0.5, 9.99);
+            totalPrice += currentPrice;
+            goContinue = InputHelper.getYNConfirm(scan, "Do you want to add another item?");
+        } while (goContinue);
+
+        System.out.printf("Total cost: $%-3.2f", totalPrice);
+    }
 }
